e43563d7c6220a200d9f01842a19e36aa385708a
Jaken <39620029+Jakentop@users.noreply.github.com>
2022-08-31 16:29:15 +0800

    Initial commit

---
+++ electron-builder.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  directories: {
+    output: 'dist/electron',
+  },
+  publish: null,
+  npmRebuild: false,
+  electronDownload: {
+    mirror: 'https://npm.taobao.org/mirrors/electron/',
+  },
+  files: [
+    'dist/main/**/*',
+    'dist/preload/**/*',
+    'dist/render/**/*',
+  ],
+}
+
+module.exports = config
